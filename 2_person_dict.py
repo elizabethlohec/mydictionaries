629629a7deae0b0a7b7c3943968303dc80deb2ae
Elizabeth <elizabeth_lohec1@baylor.edu>
2023-01-30 14:15:56 -0600

    practice

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,12 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+print(person["children"][1])
+print(person["pets"]["cat"])
+for i in person["children"]:
+    print(i)
+
+for i,j in person['pets'].items():
+    print(f"Type of Pet:{i} name of pet: {j}")
